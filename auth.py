3e67f9f251e3f39602afe0f7b2a3502dbbdb6faf
Kaash Desai <138083663+kaashdesai123@users.noreply.github.com>
2023-10-16 21:47:20 -0700

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User, db
+
+auth = Blueprint('auth', __name__)
+
+# ... Registration, login, and logout routes here ...
